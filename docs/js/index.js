07493318f30810177f9c8f2e84652492e9d71525
阎王 <yanwang.lj@alibaba-inc.com>
2017-02-07 20:22:36 +0800

    modify

--- docs/js/index.js
+++ docs/js/index.js
@@ -15244,7 +15244,7 @@ $.each(kv, function(index, item) {
 
 // 方便跨域加载资源
 if (/\.barretlee\.com$/.test(location.hostname)) {
-  document.domain = '.barretlee.com';
+  document.domain = 'barretlee.com';
 }
 
 var themes = [
